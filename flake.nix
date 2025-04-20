4636362c8d66ea80033a9f8b63907a7d9745b733
CrimsonVoid <CrimsonVoid@users.noreply.github.com>
2025-04-19 22:59:14 -0500

    [nix flake] swap to fenix for rust toolchains

--- flake.nix
+++ flake.nix
@@ -1,32 +1,31 @@
 {
-  description = "a c++ project";
+  description = "tsunami bittorrent client";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    rustovl.url = "github:oxalica/rust-overlay";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rustovl }:
+  outputs = { self, nixpkgs, fenix }:
     let
-      archs =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      archs = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       genSystems = nixpkgs.lib.genAttrs archs;
-
-      overlays = [ (import rustovl) ];
-      sysPkgs = genSystems (system: import nixpkgs { inherit system overlays; });
+      sysPkgs = genSystems (system: import nixpkgs { inherit system; });
     in
     {
       devShells = genSystems (system:
         let
           pkgs = sysPkgs.${system};
-          llvm = pkgs.llvmPackages_16;
         in
         {
           default = pkgs.mkShell {
             name = "tsunami";
 
             buildInputs = with pkgs; [
-              rust-bin.nightly.latest.default
+              fenix.packages.${system}.latest.toolchain
               pkg-config
               openssl
             ];
