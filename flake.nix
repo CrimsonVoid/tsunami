48f86bf4e4408ce4fc029c70fa92facb23d9d816
CrimsonVoid <CrimsonVoid@users.noreply.github.com>
2025-04-20 13:10:23 -0500

    minor flake.nix cleanup

--- flake.nix
+++ flake.nix
@@ -12,26 +12,23 @@
   outputs = { self, nixpkgs, fenix }:
     let
       archs = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      genSystems = nixpkgs.lib.genAttrs archs;
-      sysPkgs = genSystems (system: import nixpkgs { inherit system; });
+      sysPkgs = nixpkgs.lib.genAttrs archs (s: import nixpkgs { system = s; });
+      genSystems = fn: nixpkgs.lib.genAttrs archs (s: fn s sysPkgs.${s});
     in
     {
-      devShells = genSystems (system:
-        let
-          pkgs = sysPkgs.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            name = "tsunami";
+      devShells = genSystems (system: pkgs: {
+        default = pkgs.mkShell {
+          name = "tsunami";
 
-            buildInputs = with pkgs; [
-              fenix.packages.${system}.latest.toolchain
-              pkg-config
-              openssl
-            ];
-          };
-        });
+          buildInputs = with pkgs; [
+            fenix.packages.${system}.latest.toolchain
+            pkg-config
+            openssl
+          ];
+        };
+      });
 
-      formatter = genSystems (system: sysPkgs.${system}.nixpkgs-fmt);
+      formatter = genSystems (_: pkgs: pkgs.nixpkgs-fmt);
     };
 }
+
